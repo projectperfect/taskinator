9b03b4c96db75fc6a8f472526ee361c24b7b2bce
projectperfect <jprfct@protonmail.com>
2023-01-31 07:28:55 -0800

    form submit functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "this is a new task";
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
